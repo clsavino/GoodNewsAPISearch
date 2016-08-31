a1ce831aabc1f1e6cf44fd91a6bb85ba1da6d6be
Christi Savino <c.savino@ymail.com>
2016-08-31 17:19:51 -0400

    added firebase initialization code to the js file and set up html file with links and sources for scripts

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDGTncnIHRull__Yir9rcAeymPrEjC5nKY",
+    authDomain: "good-news-api-search.firebaseapp.com",
+    databaseURL: "https://good-news-api-search.firebaseio.com",
+    storageBucket: "good-news-api-search.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+    
