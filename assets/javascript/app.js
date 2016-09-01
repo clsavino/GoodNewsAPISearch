03b2dd31652e0946a110256b017079b139c49051
Ryan Osterman <ryanosterman10@dhcp-v880-16-00785.1Xwireless.unc.edu>
2016-09-01 18:40:00 -0400

    working on js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,4 +7,28 @@
   };
   firebase.initializeApp(config);
 
+  var searchTerm = 'good news';
+
+$.ajax(
+    "https://www.reddit.com/subreddits/search.json",
+    {
+        data: { q: searchTerm },
+        success: function(responseData) {
+            if (responseData.data.children.length > 0) {
+                console.log('# of results: ' + responseData.data.children.length);
+                $.each(responseData.data.children, function(idx, searchResult) {
+                    console.log("--- Title of Subreddit: " + searchResult.data.title);
+                    console.log(searchResult);
+                    $('.goodNews').append(searchResult.data.title);
+                });
+            } else {
+                console.log("No subreddits match the search query!");
+            }
+        },
+        error: function() {
+            alert("Something didn't work!");
+        }
+    }
+);
+
     
