42bc43530ef37535d5ff99fe9ddb7448aa0755af
Ryan Osterman <ryanosterman10@dhcp-v880-5b-00158.1Xwireless.unc.edu>
2016-09-08 20:41:47 -0400

    Revert "minor fixes"
    This reverts commit 91deb2da85298c0c99f1d4ed83b835191bd05773.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,13 +59,9 @@ $.ajax(
                     console.log(searchResult);
                     console.log("URL: " + searchResult.data.url);
                     var title = searchResult.data.title;
-                    // if (searchResult.data.thumbnail != "self" ) {
-                    //     $('#newsLinks').prepend('<div class="redditResult"><a href="' + searchResult.data.url + '">' + '<img src="' + searchResult.data.thumbnail + '"/>' + title + '</a></div>' + '<br>');
-                    // };
                     if (searchResult.data.thumbnail != "self" ) {
-                        $('#newsLinks').prepend('<div class="redditResult text-center"><div class="redditImage"><a href="' + searchResult.data.url + '" target="_blank">' + '<img src="' + searchResult.data.thumbnail + '"/></a></div>' + '<div class="redditLink"><a href="' + searchResult.data.url + '" target="_blank">' + title + '</a></div></div>' + '<br>');
+                        $('#newsLinks').prepend('<div class="redditResult"><a href="' + searchResult.data.url + '">' + '<img src="' + searchResult.data.thumbnail + '"/>' + title + '</a></div>' + '<br>');
                     };
-
                 });
             } else {
                 console.log("No subreddits match the search query!");
